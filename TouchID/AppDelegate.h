beadf266cd5cd9f8c2bd96a2c3982682b91b32b5
xiaoanning <734871221@qq.com>
2017-06-19 16:11:34 +0800

    Initial Commit

---
+++ TouchID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchID
+//
+//  Created by 安宁 on 2017/6/19.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchID
+//
+//  Created by 安宁 on 2017/6/19.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
